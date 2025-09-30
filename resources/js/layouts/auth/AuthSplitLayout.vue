25e15ae72f6eea08525c55d357efbb9120bb94f1
Nikko Niel Zamora <61865967+Nikko1798@users.noreply.github.com>
2025-09-30 16:18:17 +0800

    Improve login UI

--- resources/js/layouts/auth/AuthSplitLayout.vue
+++ resources/js/layouts/auth/AuthSplitLayout.vue
@@ -1,6 +1,11 @@
 <script setup lang="ts">
 import { onMounted, onUnmounted, ref } from "vue";
 import { usePage } from "@inertiajs/vue3";
+import bannerOne from "@/assets/images/1.jpg";
+import bannerTwo from "@/assets/images/2.jpg";
+import bannerThree from "@/assets/images/3.jpg";
+import bannerFour from "@/assets/images/4.jpg";
+import bannerFive from "@/assets/images/5.jpg";
 
 const page = usePage();
 const quote = page.props.quote;
@@ -8,13 +13,14 @@ const props=defineProps<{
     title?: string;
     description?: string;
 }>();
- const images = [
-        "https://ik.imagekit.io/tvlk/blog/2023/09/shutterstock_2317865365.jpg?tr=q-70,c-at_max,w-1000,h-800,dpr-2",
-        "https://upload.wikimedia.org/wikipedia/commons/f/f3/Manila_Metropolitan_Theater_or_commonly_called_the_Met%2C_an_abandoned_art_deco_building_in_the_heart_of_Manila.jpg",
-        "https://scontent.fmnl10-1.fna.fbcdn.net/v/t39.30808-6/505789002_1125988982905703_4831457776447996641_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=f727a1&_nc_eui2=AeEVBNQiUYXzW16jmrE6fr2YCnaCwnij3pAKdoLCeKPekHJCTU5cgL0BXhUwQwjyjmHHzqEEopElyx9AYHR7tUuj&_nc_ohc=yJCppar3IDsQ7kNvwGNHWgc&_nc_oc=AdkXTT2Hl5tWtvZ3cGUT0LC5mNqc7hiLA80G1Cjzb5UepHec8F1xWFwf1TRO_UcxRvs&_nc_zt=23&_nc_ht=scontent.fmnl10-1.fna&_nc_gid=DGD1A8O2cgc-LF4UaHVc6Q&oh=00_AfZUqEQB6lSbvXjWw-m7Ds-Wi4ylZpW58qo6qCDeEwIbmA&oe=68C5B40D",
-        "https://scontent.fmnl10-1.fna.fbcdn.net/v/t39.30808-6/486943898_1069365261901409_2322627954550863107_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=f727a1&_nc_eui2=AeGIYxAo8-7vSfBcsFgTDzAspWUYsh9vT6elZRiyH29PpymcKz_Zp2XQ19EHS5LvY2C80SNVhsOg9cvZ_PvM--uK&_nc_ohc=aNVWRzZY_6wQ7kNvwGxp6_r&_nc_oc=AdlcPR4I9mD-aYyhLQc2eXCF7HW3Ef4hM-LWHM4OW6CYoDxCoFdTmlgtWTxLzVW8xGc&_nc_zt=23&_nc_ht=scontent.fmnl10-1.fna&_nc_gid=qjPNVrPnl90h6CsfT9nsUw&oh=00_Afb6B83ZI1M6Et_moo0mziCiL6PrmkmZKJYvGi-4orN5Eg&oe=68C5B232"
-    ];
+//  const images = [
+//         "https://ik.imagekit.io/tvlk/blog/2023/09/shutterstock_2317865365.jpg?tr=q-70,c-at_max,w-1000,h-800,dpr-2",
+//         "https://upload.wikimedia.org/wikipedia/commons/f/f3/Manila_Metropolitan_Theater_or_commonly_called_the_Met%2C_an_abandoned_art_deco_building_in_the_heart_of_Manila.jpg",
+//         "https://scontent.fmnl10-1.fna.fbcdn.net/v/t39.30808-6/505789002_1125988982905703_4831457776447996641_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=f727a1&_nc_eui2=AeEVBNQiUYXzW16jmrE6fr2YCnaCwnij3pAKdoLCeKPekHJCTU5cgL0BXhUwQwjyjmHHzqEEopElyx9AYHR7tUuj&_nc_ohc=yJCppar3IDsQ7kNvwGNHWgc&_nc_oc=AdkXTT2Hl5tWtvZ3cGUT0LC5mNqc7hiLA80G1Cjzb5UepHec8F1xWFwf1TRO_UcxRvs&_nc_zt=23&_nc_ht=scontent.fmnl10-1.fna&_nc_gid=DGD1A8O2cgc-LF4UaHVc6Q&oh=00_AfZUqEQB6lSbvXjWw-m7Ds-Wi4ylZpW58qo6qCDeEwIbmA&oe=68C5B40D",
+//         "https://scontent.fmnl10-1.fna.fbcdn.net/v/t39.30808-6/486943898_1069365261901409_2322627954550863107_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=f727a1&_nc_eui2=AeGIYxAo8-7vSfBcsFgTDzAspWUYsh9vT6elZRiyH29PpymcKz_Zp2XQ19EHS5LvY2C80SNVhsOg9cvZ_PvM--uK&_nc_ohc=aNVWRzZY_6wQ7kNvwGxp6_r&_nc_oc=AdlcPR4I9mD-aYyhLQc2eXCF7HW3Ef4hM-LWHM4OW6CYoDxCoFdTmlgtWTxLzVW8xGc&_nc_zt=23&_nc_ht=scontent.fmnl10-1.fna&_nc_gid=qjPNVrPnl90h6CsfT9nsUw&oh=00_Afb6B83ZI1M6Et_moo0mziCiL6PrmkmZKJYvGi-4orN5Eg&oe=68C5B232"
+//     ];
 
+const images=[bannerOne, bannerTwo, bannerThree, bannerFour, bannerFive]
 const currentIndex = ref(0);
 
 onMounted(() => {
