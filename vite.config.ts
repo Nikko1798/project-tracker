bcb25ef086c4e7f90e306af5b8ec706ff80b8cd4
Nikko Niel Zamora <61865967+Nikko1798@users.noreply.github.com>
2025-09-30 14:03:54 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,28 +4,27 @@ import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
 import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig(({ mode }) => {
-const env = loadEnv(mode, process.cwd(), '');
-    return {
-        base: env.VITE_BASE_URL, // fallback to root
-        plugins: [
-            laravel({
-                input: ['resources/js/app.ts'],
-                ssr: 'resources/js/ssr.ts',
-                refresh: true,
-            }),
-            tailwindcss(),
-            wayfinder({
-                formVariants: true,
-            }),
-            vue({
-                template: {
-                    transformAssetUrls: {
-                        base: null,
-                        includeAbsolute: false,
-                    },
+const baseUrl = import.meta.env.VITE_BASE_URL;
+export default defineConfig({
+    
+    base: baseUrl,
+    plugins: [
+        laravel({
+            input: ['resources/js/app.ts'],
+            ssr: 'resources/js/ssr.ts',
+            refresh: true,
+        }),
+        tailwindcss(),
+        wayfinder({
+            formVariants: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
                 },
-            }),
-        ],
-    };
+            },
+        }),
+    ],
 });
