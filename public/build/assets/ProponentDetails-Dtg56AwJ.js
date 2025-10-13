c486acc06efdb5c81eb33328754b5802dc8be481
Nikko Niel Zamora <nikkoniel1@gmail.com>
2025-10-13 01:42:19 +0000

    Build assets for production

---
+++ public/build/assets/ProponentDetails-Dtg56AwJ.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./ProponentDetails.vue_vue_type_script_setup_true_lang-Bpic9SB2.js";import"./app-DUM5nK6_.js";export{m as default};
---
+++ public/build/assets/ResponsiblePersons-Dl-B-GxZ.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./ResponsiblePersons.vue_vue_type_script_setup_true_lang-Dq5lrhkB.js";import"./app-DUM5nK6_.js";export{m as default};
